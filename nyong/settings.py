abaa409f38f5c5353697fea7fdd3c57aa2944925
syp <p1645@naver.com>
2019-07-12 15:39:08 +0900

    settings.py 오타수정 및 새 앱 추가

--- nyong/settings.py
+++ nyong/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gitexercise.apps.GitexerciseConfig'.
+    'gitexercise.apps.GitexerciseConfig', 
+    'testApp.apps.TestAppsConfig',
 ]
 
 MIDDLEWARE = [
